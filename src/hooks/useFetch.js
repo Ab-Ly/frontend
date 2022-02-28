181a3315dc0e8894cae800fa562b134de2f40b5e
Ab-Ly <abdelalilyoussefi@icloud.com>
2022-02-28 22:37:16 +0100

    Project Init 📡 & Cleaning 🚽

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+      } catch (error) {}
+    };
+    fetchData();
+  });
+
+  return {
+    loading,
+    error,
+    data,
+  };
+};
+
+export default useFetch;
